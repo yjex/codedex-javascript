ff29925c54431ab1d2fcf1dfe8f94f89dcd669ca
yjex <alexistnlx91@gmail.com>
2024-07-16 17:56:51 +0800

    finish fizzbuzz checkpoint project

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,1 +1,13 @@
-let value = 15
+const userValue = 15
+
+for (let i = 1; i <= userValue; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
